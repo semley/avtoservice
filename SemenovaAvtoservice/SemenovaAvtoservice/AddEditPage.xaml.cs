2f19be4164283ca73d5657acd97cdeced91d436f
Adelina Semenova <adalyk@yandex.ru>
2023-10-20 13:05:03 +0500

    6 лаба

--- SemenovaAvtoservice/SemenovaAvtoservice/AddEditPage.xaml.cs
+++ SemenovaAvtoservice/SemenovaAvtoservice/AddEditPage.xaml.cs
@@ -20,9 +20,59 @@ namespace SemenovaAvtoservice
     /// </summary>
     public partial class AddEditPage : Page
     {
-        public AddEditPage()
+        private Service _currentServise = new Service();
+
+        public AddEditPage(Service SelectedService)
         {
             InitializeComponent();
+            if(SelectedService !=null)
+                _currentServise=SelectedService;
+            DataContext = _currentServise;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors=new StringBuilder();
+
+            if(string.IsNullOrWhiteSpace(_currentServise.Title))
+            {
+                errors.AppendLine("Укажите название услуги");
+            }
+            if(_currentServise.Cost==0)
+            {
+                errors.AppendLine("Укажите стоимость услуги");
+            }
+            if (string.IsNullOrWhiteSpace(Convert.ToString(_currentServise.Discount)))
+            {
+                errors.AppendLine("Укажите скидку");
+            }
+            if(string.IsNullOrWhiteSpace(_currentServise.DurationIn))
+            {
+                errors.AppendLine("Укажите длительность услуги");
+            }
+
+            if(errors.Length>0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentServise.ID == 0)
+                Семенова_avroservisEntities.GetContext().Service.Add(_currentServise);
+            try
+            {
+                Семенова_avroservisEntities.GetContext().SaveChanges();
+                MessageBox.Show("информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            
         }
     }
 }
--- SemenovaAvtoservice/SemenovaAvtoservice/ServisePage.xaml.cs
+++ SemenovaAvtoservice/SemenovaAvtoservice/ServisePage.xaml.cs
@@ -30,7 +30,7 @@ namespace SemenovaAvtoservice
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+            //Manager.MainFrame.Navigate(new AddEditPage());
         }
 
         private void UpdateServices()
@@ -96,6 +96,30 @@ namespace SemenovaAvtoservice
         private void RButtonUp_Checked_1(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Service));
+        }
+
+        private void EditButton_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                Семенова_avroservisEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                ServiceListView.ItemsSource = Семенова_avroservisEntities.GetContext().Service.ToList();
+            }
         }
     }
 }
